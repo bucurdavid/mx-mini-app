5760a53ec973d4fe84d642afb60e9915484434d1
Bucur David <dbucur22@gmail.com>
2024-08-07 11:04:20 +0300

    feat: added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import {nodePolyfills} from 'vite-plugin-node-polyfills'
 // import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
@@ -17,6 +18,9 @@ export default defineConfig({
     // Allows using self-signed certificates to run the dev server using HTTPS.
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
     // basicSsl(),
+    nodePolyfills({
+      globals: {Buffer: true, global: true, process: true},
+    }),
   ],
   publicDir: './public',
   server: {
