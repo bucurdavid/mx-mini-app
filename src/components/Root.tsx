f504244d306a18a8e735c970d8bd8bf8266c96d0
Bucur David <dbucur22@gmail.com>
2024-08-07 11:07:03 +0300

    fix: removed unused

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,5 @@
 import {SDKProvider, useLaunchParams} from '@telegram-apps/sdk-react'
-import {type FC, useEffect, useMemo} from 'react'
+import {type FC, useEffect} from 'react'
 
 import {App} from '@/components/App.tsx'
 import {ErrorBoundary} from '@/components/ErrorBoundary.tsx'
@@ -21,9 +21,6 @@ const ErrorBoundaryError: FC<{error: unknown}> = ({error}) => (
 
 const Inner: FC = () => {
   const debug = useLaunchParams().startParam === 'debug'
-  const manifestUrl = useMemo(() => {
-    return new URL('tonconnect-manifest.json', window.location.href).toString()
-  }, [])
 
   // Enable debug mode to see all the methods sent and events received.
   useEffect(() => {
